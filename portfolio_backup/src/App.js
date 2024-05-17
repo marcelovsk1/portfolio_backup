e78db2ede62f61e66f511255d4089bf332d061ba
Marcelo Amaral Alves <marceloaa@live.com>
2024-05-17 11:38:34 -0300

    working on

--- portfolio_backup/src/App.js
+++ portfolio_backup/src/App.js
@@ -7,7 +7,7 @@ import About from "./Components/About/About"
 import Experience from "./Components//Experience/Experience";
 // import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
-// import Footer from "./Components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
 
 
 function App() {
@@ -26,7 +26,7 @@ function App() {
       <Experience />
       {/* <Projects /> */}
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
